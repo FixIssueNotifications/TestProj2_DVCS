3b10ba4309478f53f263bd16b05b63b2cd27e5ad
Glympse-Ethan <ethan@glympse.com>
2013-01-03 13:01:04 -0800

    edit from windows

--- TestProj2_DVCS/GLYFirstViewController.m
+++ TestProj2_DVCS/GLYFirstViewController.m
@@ -19,7 +19,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.title = NSLocalizedString(@"First", @"First");
+        self.title = NSLocalizedString(@"First!", @"Firstzzz");
         self.tabBarItem.image = [UIImage imageNamed:@"first"];
     }
     return self;
